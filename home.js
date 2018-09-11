cf31fce5550c1982cee5359b1929aa9bb6a858b7
Jackie Liu <jlliu@mit.edu>
2018-09-10 21:29:34 -0400

    updating

--- home.js
+++ home.js
@@ -50,9 +50,91 @@ $(document).ready(function(){
 	}
 
 	$(".bg-container").append(listOfIcons);
-	$(".bg-container").fadeIn(2000);
+	$(".bg-container").fadeIn(2000,function(){
+		$('.hero-arrow').css("opacity","1");
+	});
+	
 
 
+
+
+	function isScrolledIntoView(el) {
+		    var rect = el.getBoundingClientRect();
+		    var elemTop = rect.top;
+		    var elemBottom = rect.bottom;
+
+		    // Only completely visible elements return true:
+		    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+
+		    return isVisible;
+		
+			// $(".powrMarkLink").css('visibility','hidden');
+		}
+
+		//Runs on scroll and updates which Section to make active in the header
+		var activeHeaderId = null;
+
+		function updateHeader() {
+			$.each($(".nav ul li a"), function(index,value){
+				var thisLink = value;
+				var id = $(thisLink).attr("id")
+				// console.log("id"+id);
+				//The top or bottom boundary has just scrolled into view, and its ID is not the active one
+				if ($("boundary"+"#"+id) != null) {
+					if ((isScrolledIntoView($("boundary"+"#"+id)[0]) || isScrolledIntoView($("boundary"+"#"+id)[1]))  && id != activeHeaderId){
+						// console.log("test");
+						$(".nav a").not("a#"+id).removeClass("active");
+						var activeHeaderId = id;
+						$("a#"+id).addClass("active");
+					}
+				}
+
+			});
+
+		};
+
+		
+		console.log("poop");
+
+		// $(".nav").sticky({topSpacing:0});
+
+		document.addEventListener("scroll", updateHeader);
+
+		const $navbar = $('.nav');
+
+		$('a[href^="#"]').on('click', function(e) {
+		    e.preventDefault();
+		    const scrollTop =
+		        $($(this).attr('href')).position().top -
+		        $navbar.outerHeight();
+
+		    $('html, body').animate({ scrollTop });
+		    var id = $(this).attr("id")
+    		$(".nav a").not("a#"+id).removeClass("active");
+			var activeHeaderId = id;
+			$("a#"+id).addClass("active");
+
+		});
+
+
+		$('.hero-arrow').on('click', function(e) {
+			console.log("hero clicked");
+		    $("#aboutSection").trigger("click");
+
+		})
+
+
+		// var animation = bodymovin.loadAnimation({
+		// 	container: document.getElementById('bm'),
+		// 	renderer: 'svg',
+		// 	loop:true,
+		// 	autoplay: true,
+		// 	path: 'heart-mail.json'
+		// })
+
+		// addEventListener('hover',function(){
+		//   myAnimationElement.beginElement();
+		// },false);
 
 
 });
---
+++ work-page.js
@@ -0,0 +1,44 @@
+
+	console.log("this is external script");
+
+	function populatePage(entryId){
+		console.log('populate page');
+
+		var client = contentful.createClient({
+		  space: 't9p4c9x03bar',
+		  accessToken: '13f8aeca80c777b1e2991e2cfe37175daba1fd946552e12e57bb93097c221bc4'
+		})
+
+
+		client.getEntry(entryId)
+		.then(function (entry) {
+		  // logs the entry metadata
+		  console.log(entry.sys)
+
+		  // logs the field with ID title
+		  console.log(entry.fields.workTitle);
+		  console.log(entry.fields.description);
+
+		  var workTitle = entry.fields.workTitle;
+		  var workType = entry.fields.workType;
+		  var doneFor = entry.fields.doneFor;
+		  var toolsUsed = entry.fields.toolsUsed;
+		  var timePeriod = entry.fields.timePeriod;
+
+		  console.log([workTitle,workType,doneFor,toolsUsed,timePeriod]);
+
+		  var description = marked(entry.fields.description);
+
+
+		  	$("#doneFor").html(doneFor);
+		  	$("#workType").html(workType);
+		  	$("#timePeriod").html(timePeriod);
+		  	$("#toolsUsed").html(toolsUsed);
+			$(".title").html(workTitle);
+			$(".description").html(description);
+
+		})
+
+	};
+
+
