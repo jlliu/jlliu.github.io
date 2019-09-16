27d7af7dd27df32b28a3a5ee5c165cccfadbc846
Jackie Liu <jlliu@mit.edu>
2019-09-16 15:48:33 -0400

    Revert "MOVING IT TO A GRIDSOME SITE"
    This reverts commit d029f61480c51e04224263697fa5eda52570221f.

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "siteUrl": "",
-  "siteName": "Jackie Liu",
-  "titleTemplate": "%s - Jackie Liu",
-  "siteDescription": "",
-  "version": "0.6.2"
-}
--- src/.temp/plugins-server.js
+++
@@ -1,8 +0,0 @@
-import plugin_project_5 from "/Users/jackie/Dropbox (Personal)/jlliu.github.io/gridsome.client.js"
-
-export default [
-  {
-    run: plugin_project_5,
-    options: {}
-  }
-]
