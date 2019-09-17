7fb9d51bddfef0e5f4f8a61e978d9b8eced8b171
Jackie Liu <jlliu@mit.edu>
2019-09-17 00:52:14 -0400

    Updates

--- src/.temp/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "titleTemplate": "%s - jlliu.github",
-  "siteUrl": "https://jackieis.online",
-  "version": "0.7.3"
-}
--- src/.temp/plugins-server.js
+++
@@ -1,8 +0,0 @@
-import plugin_project_6 from "/Users/jackie/Dropbox (Personal)/jlliu.github.io/gridsome.client.js"
-
-export default [
-  {
-    run: plugin_project_6,
-    options: {}
-  }
-]
